83baea2987d0bc4836102ee5c98c34975c9ad44f
TMATS8 <maddoxmking@gmail.com>
2024-12-19 21:18:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     const songImages = [
-        'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228', // Example image 1
+        'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-01D7DC796B915594F5AF682133542CD9-Png/150/150/AvatarHeadshot/Webp/noFilter', // Example image 1
         'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228', // Example image 2
         'https://i.scdn.co/image/ab67616d0000b273c67bbfd9cb198520f456f04d', // Example image 3
         'https://i.scdn.co/image/ab67616d0000b2738133cb00ae0c3de2381104e0', // Example image 4
