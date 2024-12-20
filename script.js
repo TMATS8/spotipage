bdf40df56a046d5ed38f50f64b38e062f83ddfad
TMATS8 <maddoxmking@gmail.com>
2024-12-19 21:19:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 $(document).ready(function () {
     const songImages = [
-        'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-01D7DC796B915594F5AF682133542CD9-Png/150/150/AvatarHeadshot/Webp/noFilter', // Example image 1
-        'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228', // Example image 2
+        'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228', // Example image 1
+        'https://i.scdn.co/image/ab67616d0000b273f8ebde9d9d7b29093f3b76d1', // Example image 2
         'https://i.scdn.co/image/ab67616d0000b273c67bbfd9cb198520f456f04d', // Example image 3
         'https://i.scdn.co/image/ab67616d0000b2738133cb00ae0c3de2381104e0', // Example image 4
         'https://i.scdn.co/image/ab67616d0000b273c6a219c0de3d1c0f7cd378fc'  // Example image 5
     ];
 
-    // Populate the songs container with image elements
     const songsContainer = $('#songs-container');
+    
+    // Clear the container before adding new images
+    songsContainer.empty();
+
+    // Populate the songs container with image elements
     songImages.forEach((imageUrl) => {
         const songItem = `<div class="song-item">
-            <img src="${imageUrl}" alt="Song Image" />
+            <img src="${imageUrl}" alt="Song Image" onError="this.onerror=null;this.src='fallback_image.jpg';" />
         </div>`;
         songsContainer.append(songItem);
     });
